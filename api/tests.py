886cafe46c9ed932d3344476b63023207071f0c8
kish38 <kishorekumarnetala@gmail.com>
2019-06-29 11:10:07 +0530

    basic home model tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
+from django.urls import reverse
 from django.test import TestCase
+from api.models import Task
 
-# Create your tests here.
+
+class BasicTests(TestCase):
+
+    def test_home(self):
+        req = self.client.get(reverse('home'))
+        self.assertEqual(req.status_code, 200)
+
+
+class TaskAPITests(TestCase):
+
+    def setUp(self):
+        Task.objects.create(name="Task one")
+        Task.objects.create(name="Task two")
+
+    def test_model_create(self):
+        tasks = Task.objects.count()
+        self.assertEqual(tasks, 2)
+        task = Task.objects.create(name="Third task")
+        self.assertEqual(Task.objects.count(), 3)
+        self.assertEqual(task.completed, False)
